7925568d738e3aba93d6059cc894fa20b93229f8
Falk Winkler <fwinkler0573@gmail.com>
2017-11-04 14:36:52 +0100

    Login Dialog erstellt

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,11 +1,50 @@
 import * as React from 'react'
-import { Panel } from '@extjs/reactor/modern';
+import { Container,TitleBar ,Panel, TextField , Button, Menu,MenuItem, Dialog,MessageBox } from '@extjs/reactor/modern';
 import { renderWhenReady } from '@extjs/reactor';
 
 class Layout extends React.Component {
+
+    state = {
+        showDialog: false
+    }
+
+    onLoginClicked = () => { 
+        MessageBox.bind(this,"Test");
+        console.log("button clicked");       
+    }
+
+    showDialog = () => {
+        this.setState({ showDialog: true });
+    }
+    onOk = () => {
+        this.setState({ showDialog: false });
+    }
+    onCancel = () => {
+        this.setState({ showDialog: false });
+    }
+
     render() {
+        const { showDialog } = this.state;
         return (
-            <Panel title="Ext JS">Hello World!</Panel>
+            <Container fullscreen layout="fit"> 
+                <TitleBar title="Identityservice" docked="top" >
+                    <Button margin="10" docked="right" titel="Login" text="Login" handler={this.showDialog}  ></Button>
+                </TitleBar>
+                <Dialog                 
+                 title="Login"
+                 closeAction="hide"
+                 maskTapHandler={this.onCancel} 
+                 displayed={showDialog}
+                 onHide={() => this.setState({ showDialog: false })}
+                 >
+
+                 <TextField label="Username" inputType="textfield"></TextField>
+                 <TextField label="Password" inputType="password" ></TextField>
+                
+                    <Button text="Cancel" handler={this.onCancel}/>
+                    <Button itemId="ok" text="OK" handler={this.onOk}/>
+                </Dialog>
+            </Container>           
         )
     }
 }
